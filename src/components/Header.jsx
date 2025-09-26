9ffc63d6094a11f8036d7f4226611c375b175f58
salman-itShaala <salman.shaikh@itshaala.com>
2025-09-26 13:10:09 +0530

    half completed taste viebes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,69 @@ import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import { VscThreeBars } from "react-icons/vsc";
 import { FaHamburger } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [recipeInput, setRecipeInput] = useState("");
+  console.log(recipeInput);
+  const [recipeList, setRecipeList] = useState([]);
+
+  useEffect(() => {
+    // https://dummyjson.com/recipes/search?q=classic
+
+    if (recipeInput === "") {
+      return;
+    }
+
+    searchRecipes();
+
+    async function searchRecipes() {
+      const res = await fetch(
+        `https://dummyjson.com/recipes/search?q=${recipeInput}`
+      );
+      const data = await res.json();
+
+      setRecipeList(data.recipes);
+    }
+  }, [recipeInput]);
+
   return (
-    <header className="flex text-2xl items-center justify-between  max-w-7xl h-20 m-auto">
+    <header className="flex relative text-2xl items-center justify-between h-20 px-10">
       <Link to="/" className="flex items-center gap-4 font-bold">
         <FaHamburger />
         Taste Vibes
       </Link>
 
       <div className="flex gap-4">
-        <button className="cursor-pointer">
+        <button className="cursor-pointer" onClick={() => setIsOpen(true)}>
           <CiSearch />
         </button>
         <button className="cursor-pointer">
           <VscThreeBars />
         </button>
       </div>
+      {isOpen && (
+        <div className="absolute top-0 left-0 right-0 bg-slate-600 flex justify-center items-center h-screen w-full">
+          <div className="h-[60vh] w-[40vw] border p-4">
+            <button onClick={() => setIsOpen(close)}>Close</button>
+            <input
+              type="search"
+              className="border w-full"
+              placeholder="Search recipe..."
+              onChange={(e) => setRecipeInput(e.target.value)}
+              value={recipeInput}
+            />
+
+            <div className="flex flex-col">
+              {recipeList.map((recipe) => {
+                return <Link to={`/recipe/${recipe.id}`}>{recipe.name}</Link>;
+              })}
+            </div>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
--- src/components/HotRecipes.jsx
+++ src/components/HotRecipes.jsx
@@ -1,23 +1,34 @@
 import { useEffect, useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { Link } from "react-router-dom";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const HotRecipes = () => {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [recipeList, setRecipeList] = useState([]);
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
     getRecipes();
 
     async function getRecipes() {
       // to do -- add try catch
-      const res = await fetch(
-        "https://dummyjson.com/recipes?limit=4&skip=0&select=name,image"
-      );
+      try {
+        const res = await fetch(
+          "https://dummyjson.com/recipes?limit=4&skip=0&select=name,image"
+        );
 
-      const data = await res.json();
+        const data = await res.json();
 
-      console.log(data.recipes);
-
-      setRecipes(data.recipes);
+        setRecipes(data.recipes);
+      } catch (error) {
+        console.log(error);
+        alert("Someting went wrong");
+      } finally {
+        setLoading(false);
+      }
     }
   }, []);
 
@@ -34,18 +45,48 @@ const HotRecipes = () => {
       </p>
 
       <div className="flex gap-8 py-8">
-        {recipes.map((recipe) => {
-          return (
-            <Link
-              key={recipe.id}
-              to={`/recipe/${recipe.id}`}
-              className="rounded-2xl p-4 border flex flex-col gap-4"
-            >
-              <img className="rounded-2xl border" src={recipe.image} alt="" />
-              <p className="font-bold text-xl">{recipe.name}</p>
-            </Link>
-          );
-        })}
+        {loading ? (
+          <>
+            {[1, 2, 3, 4].map((el) => {
+              return (
+                <div className="rounded-2xl">
+                  <div>
+                    <Skeleton
+                      width={300}
+                      height={340}
+                      baseColor="#202020"
+                      highlightColor="#444"
+                    />
+                  </div>
+                  <div>
+                    <Skeleton
+                      width={300}
+                      baseColor="#202020"
+                      highlightColor="#444"
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          recipes.map((recipe) => {
+            return (
+              <Link
+                key={recipe.id}
+                to={`/recipe/${recipe.id}`}
+                className="rounded-2xl p-4 border flex flex-col gap-4 h-[340px] w-2xl"
+              >
+                <img
+                  className="rounded-2xl border h-[300px] "
+                  src={recipe.image}
+                  alt=""
+                />
+                <p className="font-bold text-xl">{recipe.name}</p>
+              </Link>
+            );
+          })
+        )}
       </div>
     </div>
   );
